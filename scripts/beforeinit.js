eea5c67e0f090b5586fd692f72c045ba680e9d47
Slava Katiukha <v.katiukha@gmail.com>
2020-09-30 17:21:39 +0300

    fix: action function get not found

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -1,26 +1,27 @@
-  var values = jps.settings.fields[0].values;
+var values = jps.settings.fields[0].values;
   
-  function getVersions(edition, url) {
-      var body = toNative(new com.hivext.api.core.utils.Transport().get(url));
-      var arr = eval(body.substring(10, body.length - 1)); 
-      for (var i in arr) {
-        if (arr[i].edition == edition && arr[i].platform == "Unix") {
-          if (edition == "Standard") { 
-          caption = "Latest";
-          } else { 
-          caption = "Long Term Support";
-          jps.settings.fields[0]["default"] = arr[i].version;
-          }
-              values.push({ caption: arr[i].version+' '+'('+caption+')',value: arr[i].version }); 
-      <!-- Use the arr[i].zipUrl to get URL to the application  -->    
-          break;
+function getVersions(edition, url) {
+  var body = toNative(new com.hivext.api.core.utils.Transport().get(url));
+  var arr = eval(body.substring(10, body.length - 1)); 
+  for (var i in arr) {
+    if (arr[i].edition == edition && arr[i].platform == "Unix") {
+        if (edition == "Standard") { 
+            caption = "Latest";
+        } else { 
+            caption = "Long Term Support";
+            jps.settings.fields[0]["default"] = arr[i].version;
         }
-      }
+        values.push({ caption: arr[i].version+' '+'('+caption+')',value: arr[i].version });       
+        break;
+    }
   }
-  getVersions("Standard",  "https://my.atlassian.com/download/feeds/current/jira-software.json");
-  getVersions("Enterprise", "https://my.atlassian.com/download/feeds/archived/jira-software.json");
-  if (values.length > 0) {  
-    return jps;    
-  } else {
-    return {result: 1099, error:"can't determine latest or lts version", versions: values };
-  }
+}
+  
+getVersions("Standard",  "https://my.atlassian.com/download/feeds/current/jira-software.json");
+getVersions("Enterprise", "https://my.atlassian.com/download/feeds/archived/jira-software.json");
+  
+if (values.length > 0) {  
+  return { result: 0, settings: jps.settings };    
+}
+
+return { result: 1099, error:"can't determine latest or lts version", versions: values };
