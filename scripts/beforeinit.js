d521ab354be3ba91b3c873a40370e1166ae32bd1
alexey.lazarenko <dzotic9@gmail.com>
2021-09-24 15:48:58 +0300

    JE-59951 [Jira]

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -1,34 +1,37 @@
-  var values = jps.settings.fields[0].values;
-  var tags = toNative(new com.hivext.api.core.utils.Transport().get("https://registry.hub.docker.com/v1/repositories/atlassian/jira-software/tags"));
-  var tagsLength = tags.length;
+import com.hivext.api.core.utils.JSONUtils;  
+
+var values = jps.settings.fields[0].values;
+var tags = toNative(new com.hivext.api.core.utils.Transport().get("https://registry.hub.docker.com/v1/repositories/atlassian/jira-software/tags"));
+var tagsLength = tags.length;
   
-  function getVersions(edition, url) {
-    var body = toNative(new com.hivext.api.core.utils.Transport().get(url));
-    var arr = eval(body.substring(10, body.length - 1)); 
-    for (var i in arr) {
-      if (arr[i].edition == edition && arr[i].platform == "Unix") {
-          if (edition == "Standard") { 
-              caption = "Latest";
-          } else { 
-              caption = "Long Term Support";
-              jps.settings.fields[0]["default"] = arr[i].version;
+function getVersions(edition, url) {
+  var body = String(new com.hivext.api.core.utils.Transport().get(url));
+  var length = body.length;
+  var arr = JSON.parse(body.substring(10, length -1));
+  for (var i in arr) {
+    if (arr[i].edition == edition && arr[i].platform == "Unix") {
+        if (edition == "Standard") { 
+            caption = "Latest";
+        } else { 
+            caption = "Long Term Support";
+            jps.settings.fields[0]["default"] = arr[i].version;
+        }
+        for (var k = 0, l = tagsLength; k < l; k++) {
+          if (tags[k].name == arr[i].version) {
+            values.push({ caption: arr[i].version+' '+'('+caption+')',value: arr[i].version });
+            break;
           }
-          for (var k = 0, l = tagsLength; k < l; k++) {
-            if (tags[k].name == arr[i].version) {
-              values.push({ caption: arr[i].version+' '+'('+caption+')',value: arr[i].version });
-              break;
-            }
-          }
-          break;
-      }
+        }
+        break;
     }
   }
+}
 
-  getVersions("Standard",  "https://my.atlassian.com/download/feeds/current/jira-software.json");
-  getVersions("Enterprise", "https://my.atlassian.com/download/feeds/archived/jira-software.json");
+getVersions("Standard",  "https://my.atlassian.com/download/feeds/current/jira-software.json");
+getVersions("Enterprise", "https://my.atlassian.com/download/feeds/archived/jira-software.json");
 
-  if (values.length > 0) {  
-    return { result: 0, settings: jps.settings };    
-  }
+if (values.length > 0) {  
+  return { result: 0, settings: jps.settings };    
+}
 
-  return { result: 1099, error:"can't determine latest or lts version", versions: values };
+return { result: 1099, error:"can't determine latest or lts version", versions: values };
